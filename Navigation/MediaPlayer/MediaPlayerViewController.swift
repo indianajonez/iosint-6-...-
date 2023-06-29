0ab0522f50fa3920881b9923d85f767d886657bc
Indiana Jonez <indiana.jonez@yandex.ru>
2023-06-29 12:01:22 +0300

    hot fix with rus

--- Navigation/MediaPlayer/MediaPlayerViewController.swift
+++ Navigation/MediaPlayer/MediaPlayerViewController.swift
@@ -15,15 +15,27 @@ class MediaPlayerViewController: UIViewController {
     
     var coordinator: CoordinatorProtocol?
     
+    
     // MARK: - Private properties
     
-   // private var mediaPlayerDelegate: MediaPlayerViewControllerDelegate
+    private let largeConfig = UIImage.SymbolConfiguration(scale: .large)
     
+    private var player = AVPlayer()
     private var audioPlayer: AVAudioPlayer!
+    private let path1 = Bundle.main.path(forResource: "Louis Armstrong", ofType: "mp3")
+    private lazy var url1 = URL(fileURLWithPath: path1 ?? "")
+    private let path2 = Bundle.main.path(forResource: "ChicagoIfYouMeNow", ofType: "mp3")
+    private lazy var url2 = URL(fileURLWithPath: path2 ?? "")
+    private let path3 = Bundle.main.path(forResource: "RichardMarxShouldKnowBetter", ofType: "mp3")
+    private lazy var url3 = URL(fileURLWithPath: path3 ?? "")
+    private let path4 = Bundle.main.path(forResource: "TheMidnightRiverOfDarkness", ofType: "mp3")
+    private lazy var url4 = URL(fileURLWithPath: path4 ?? "")
+    private lazy var item1 = AVPlayerItem(url: url1)
+    private lazy var item2 = AVPlayerItem(url: url2)
+    private lazy var item3 = AVPlayerItem(url: url3)
+    private lazy var item4 = AVPlayerItem(url: url4)
+    private lazy var playlist = [item1, item2, item3, item4]
     private var counter = 0
-    //private let systemSounds: [String: SystemSoundID] = ["SMSReceived": 1003, "CalendarAlert": 1005, "MailReceived": 1000, "LowPower": 1006]
-    private var someSongs: [String: Int] = ["Louis Armstrong": 0, "ChicagoIfYouMeNow": 1, "RichardMarxShouldKnowBetter": 2, "WhamChristmas" : 3, "TheMidnightVampires" : 4, "TheMidnightRiverOfDarkness" : 5]
-    
     private lazy var soundNameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -37,18 +49,28 @@ class MediaPlayerViewController: UIViewController {
     private lazy var playButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setImage(UIImage(systemName: "play"), for: .normal)
-        //button.tintColor = .black
+        button.setImage(UIImage(systemName: "play.circle", withConfiguration: largeConfig), for: .normal)
+        button.tintColor = .black
         button.addTarget(self, action: #selector(playButtonAction), for: .touchUpInside)
         return button
     }()
     
+    
     private lazy var stopButton: UIButton = {
         let button = UIButton() 
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setImage(UIImage(systemName: "stop.circle"), for: .normal)
-        //button.tintColor = .black
+        button.setImage(UIImage(systemName: "stop.circle", withConfiguration: largeConfig), for: .normal)
+        button.tintColor = .black
         button.addTarget(self, action: #selector(stopButtonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var forwardButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "forward.circle", withConfiguration: largeConfig), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(forwardButtonAction), for: .touchUpInside)
         return button
     }()
     
@@ -66,25 +88,34 @@ class MediaPlayerViewController: UIViewController {
 
     // MARK: - Public methods
     
-    @objc private func playButtonAction(_ sender: Any) {
-        print("fgf")
-        if audioPlayer.isPlaying {
-            audioPlayer.pause()
-            print("pause")
-            playButton.setImage(UIImage(named: "play.rectangle"), for: .normal)
-        } else {
-            audioPlayer.play()
-            print ("play")
-            playButton.setImage(UIImage(named: "pause.rectangle"), for: .normal)
-        }
-       // playSystemSound()
+    @objc private func playButtonAction() {
+        player.play()
+//        if audioPlayer.isPlaying {
+//            audioPlayer.pause()
+//            playButton.setImage(UIImage(systemName: "play.rectangle"), for: .normal)
+//        } else {
+//            audioPlayer.play()
+//            playButton.setImage(UIImage(systemName: "pause.rectangle"), for: .normal)
+//        }
     }
     
-    @objc private func stopButtonAction(_ sender: Any) { // скорее всего ошибка!
-        if audioPlayer.isPlaying {
-            audioPlayer.stop()
-            print("stop")
-        }
+    @objc private func stopButtonAction() {
+//        if audioPlayer.isPlaying {
+//            audioPlayer.stop()
+//            print("stop")
+//        }
+        player.pause()
+        player.seek(to: .zero)
+    }
+    
+    @objc private func forwardButtonAction() {
+        guard let currentItem = player.currentItem else { return }
+        guard let currentIndex = playlist.firstIndex(of: currentItem) else { return }
+        let nextIndex = (currentIndex + 1) % playlist.count
+        let nextItem = playlist[nextIndex]
+        player.seek(to: .zero)
+        player.replaceCurrentItem(with: nextItem)
+        player.play()
     }
     
     
@@ -94,44 +125,23 @@ class MediaPlayerViewController: UIViewController {
         view.backgroundColor = .white
     }
     
-//    private func playSystemSound() {
-//        let currentSoundId = Array(someSongs.values)[counter]
-//        let currentSoundName = Array(someSongs.keys)[counter]
-//
-//        AudioServicesPlaySomeSongs(currentSoundId)
-//        soundNameLabel.text = currentSoundName
-//
-//        if counter == someSongs.count - 1 {
-//            counter = 0
-//        } else {
-//            counter += 1
-//        }
-//    }
-    
     private func setupAudioPlayer() {
-        guard let musicUrl = Bundle.main.url(forResource: "Louis Armstrong", withExtension: "mp3") else { return }
-        do {
-            try audioPlayer = AVAudioPlayer(contentsOf: musicUrl)
-//                    let currentSoundId = Array(someSongs.values)[counter]
-//                    let currentSoundName = Array(someSongs.keys)[counter]
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
-    private func setupAudioSession() {
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(.playback)
-        } catch {
-            print(error.localizedDescription)
-        }
+//        guard let musicUrl = Bundle.main.url(forResource: "Louis Armstrong", withExtension: "mp3") else { return }
+//        do {
+//            try audioPlayer = AVAudioPlayer(contentsOf: musicUrl)
+////                    let currentSoundId = Array(someSongs.values)[counter]
+////                    let currentSoundName = Array(someSongs.keys)[counter]
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+        player.replaceCurrentItem(with: playlist.randomElement())
     }
     
     private func addSubView() {
         view.addSubview(soundNameLabel)
         view.addSubview(playButton)
         view.addSubview(stopButton)
+        view.addSubview(forwardButton)
     }
     
     private func setupConstraints() {
@@ -140,15 +150,15 @@ class MediaPlayerViewController: UIViewController {
             soundNameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
 
             playButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
-            playButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            playButton.heightAnchor.constraint(equalToConstant: 100),
-            playButton.widthAnchor.constraint(equalToConstant: 100),
+            playButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 70),
+//            playButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             
             stopButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
-//            stopButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -20),
-            stopButton.rightAnchor.constraint(equalTo: playButton.leftAnchor),
-            stopButton.heightAnchor.constraint(equalToConstant: 100),
-            stopButton.widthAnchor.constraint(equalToConstant: 100)
+            stopButton.trailingAnchor.constraint(equalTo: playButton.trailingAnchor, constant: 100),
+            
+            forwardButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            forwardButton.leadingAnchor.constraint(equalTo: playButton.leadingAnchor, constant: 50),
+            
         ])
     }
     
